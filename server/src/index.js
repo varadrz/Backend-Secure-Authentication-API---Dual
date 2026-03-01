96972ac9d62a162f32e1abb819b703af1b29ba84
varadrz <wm.varad.patil@gmail.com>
2026-03-01 20:01:21 +0530

    revert: remove vercel deployment parts

--- server/src/index.js
+++ server/src/index.js
@@ -66,11 +66,7 @@ app.use((req, res) => {
 });
 
 // Start Server
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () =>
-        console.log(`Server running on port ${PORT}`)
-    );
-}
-
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+    console.log(`Server running on port ${PORT}`)
+);
